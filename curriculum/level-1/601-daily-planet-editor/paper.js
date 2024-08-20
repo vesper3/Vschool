02b7052fd0a736f6baca0934da35ea64541f1c6e
Jeremy Norwood <jeremyjnorwood@gmail.com>
2024-08-20 00:39:38 -0600

    moved paper.js

--- curriculum/level-1/601-daily-planet-editor/paper.js
+++ curriculum/level-1/601-daily-planet-editor/paper.js
@@ -9,46 +9,42 @@ just fix the existing ones.
 
 // Written by Kent, Clark
 
-var Enemies=["Lex","Batman","Darkseid","Brainiac","General Zod","Doomsday"];
+var enemies = ["Lex", "Batman", "Darkseid", "Brainiac", "General Zod", "Doomsday"];
 
-funtion Who_Wins(is-there-kryptonite,enemy_name)
-{
-if (!is-there-kryptonite)
-{
-    return "Superman beats " +enemyName+ ", of course";
-}
-
-else{
-    return "Depends on how quick Superman can get rid of the Kryptonite. "+ enemyname +" could possibly win this one.";
-}
-    }
-
-for (var i=0;i<enemies.length;i++)
-{
-    var is_there_kryptonite:
-    if (i%2=== 0) {
-        is_there_kryptonite = true;
+function whoWins(isThereKryptonite, enemyName) {
+    if (!isThereKryptonite) {
+        return "Superman beats " + enemyName + ", of course";
     } else {
-        is_there_kryptonite = false;
+        return "Depends on how quick Superman can get rid of the Kryptonite. " + enemyName + " could possibly win this one.";
     }
-    console.log(Who_Wins(is_there_kryptonite,enemies[i]));
 }
 
-function HowAttractedIsLoisLaneToMe ()
-{
+for (var i = 0; i < enemies.length; i++) {
+    var isThereKryptonite;
+    if (i % 2 === 0) {
+        isThereKryptonite = true;
+    } else {
+        isThereKryptonite = false;
+    }
+    console.log(whoWins(isThereKryptonite, enemies[i]));
+}
+
+function howAttractedIsLoisLaneToMe() {
     // 1 is not at all attracted, 10 is "super" attracted...
-    return Math.floor((Mat.random()*10)+1);
+    return Math.floor((Math.random() * 10) + 1);
 }
 
-consol.log( HowAttractedIsLoisLaneToMe ()) ;
+console.log(howAttractedIsLoisLaneToMe());
 
-var clarkKent =true;
-var superman false;
+var clarkKent = true;
+var superman = false;
 
-while (clarkKent){
+while (clarkKent) {
     console.log("I'm just a nerdy columnist");
-    var phoneBoothQuickChange =Math.random();
-    if (phoneBoothQuickChange>= 0.5) {
-        clarkKent=false;
-        superman=true;
-        console.log("Now I'm Superman!");}}
+    var phoneBoothQuickChange = Math.random();
+    if (phoneBoothQuickChange >= 0.5) {
+        clarkKent = false;
+        superman = true;
+        console.log("Now I'm Superman!");
+    }
+}
