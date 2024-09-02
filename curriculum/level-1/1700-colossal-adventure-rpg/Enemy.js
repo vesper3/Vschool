2811acb613ed3463f96dad5b9a5fd24a0d830db1
Jeremy Norwood <jeremyjnorwood@gmail.com>
2024-09-02 12:43:29 -0600

    add a heal method to enemy class to prevent crashes on healing enemy

--- curriculum/level-1/1700-colossal-adventure-rpg/Enemy.js
+++ curriculum/level-1/1700-colossal-adventure-rpg/Enemy.js
@@ -2,6 +2,7 @@
 class Enemy {
   constructor(name, hp, minDamage, maxDamage) {
     this.name = name;
+    this.maxHP = hp;
     this.hp = hp;
     this.minDamage = minDamage;
     this.maxDamage = maxDamage;
@@ -16,6 +17,16 @@ class Enemy {
   takeDamage(damage) {
     this.hp -= damage;
     console.log(`Enemy HP: ${this.hp}`);
+  }
+
+  heal(amount) {
+    this.hp += amount;
+    
+    if (this.hp > this.maxHP) {
+      this.hp = this.maxHP;
+    }
+    
+    console.log(`${this.name} gains ${amount} HP! HP: ${this.hp}`);
   }
 }
 
